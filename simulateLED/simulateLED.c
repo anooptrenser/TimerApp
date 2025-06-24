45d08aa4e7f8cc0e8751967691ec1f2be2499f09
Anoop <anoop.krishnan@trenser.com>
2025-06-24 06:04:03 +0000

    Anoop - Added changes for simulating LED

---
+++ simulateLED/simulateLED.c
@@ -0,0 +1,43 @@
+//**************************** Timer App **************************************
+// Copyright (c) 2025 Trenser Technology Solutions
+// All Rights Reserved
+//*****************************************************************************
+//
+// File     : simulateLED.c
+// Summary  : Simulates LED turning ON or OFF
+// Note     : None
+// Author   : Anoop G
+// Date     : 24/06/2025
+//
+//*****************************************************************************
+
+//***************************** Include Files *********************************
+#include <stdio.h>
+#include <stdbool.h>
+
+//*****************************.DisplayLedStatus.******************************
+// Purpose  : Simulates an LED turning ON and OFF by printing "LED ON"
+//            and "LED OFF" alternately.
+// Inputs   : None
+// Outputs  : None
+// Return   : None
+// Notes    : Uses a static variable to toggle the LED state.
+//            Intended to be called repeatedly to simulate blinking.
+//*****************************************************************************
+void DisplayLedStatus(void)
+{
+    static bool blLedOn = false; // Static variable to maintain state
+
+    if (blLedOn)
+    {
+        printf("LED ON\n");
+    }
+    else
+    {
+        printf("LED OFF\n");
+    }
+
+    blLedOn = !blLedOn; // Toggle state for next call
+}
+
+// EOF
